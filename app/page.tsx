ce80723861021859870acf0bd507516c7347c2d2
Alex Thomas <Alex-thomas92@outlook.com>
2024-08-20 09:33:45 +0100

    confetti working

--- app/page.tsx
+++ app/page.tsx
@@ -49,11 +49,6 @@ const ScratchCardComponent = ({
   const handleComplete = () => {
     setIsRevealed(true);
     onReveal();
-    if (scratchedPercentage >= 70) {
-      // Check if 70% is scratched
-      // Trigger confetti
-      setConfetti(true);
-    }
   };
 
   return (
@@ -62,14 +57,14 @@ const ScratchCardComponent = ({
       <ScratchCard
         width={300}
         height={300}
-        finishPercent={70}
+        finishPercent={50}
         onComplete={handleComplete}
         customBrush={{
           width: 40,
           height: 40,
           shape: "circle",
         }}
-        onScratch={(percentage) => setScratchedPercentage(percentage)} // Update scratched percentage
+        onScratch={(percentage) => setScratchedPercentage(percentage)}
       >
         <div className="flex items-center justify-center w-full h-full bg-purple-500">
           <div className="text-center">
@@ -91,28 +86,6 @@ const ScratchCardComponent = ({
           </div>
         </div>
       </ScratchCard>
-      {!isRevealed && (
-        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-purple-500 pointer-events-none">
-          <div className="text-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-16 w-16 mx-auto mb-4 text-yellow-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-              />
-            </svg>
-            <p className="text-white text-lg font-bold">YOU WIN</p>
-            <p className="text-yellow-400 text-2xl font-bold">{prize}</p>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
